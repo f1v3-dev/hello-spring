00924b38b7df228a82d536792fd04bc51179e71b
f1v3-dev <f1v3_dev@naver.com>
2023-02-15 00:10:31 +0900

    commit test

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,6 +3,8 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// test
 @SpringBootApplication
 public class HelloSpringApplication {
 
