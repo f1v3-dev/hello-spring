d7aa137bf6727d2cceb78f3deffc15a261701d9f
f1v3-dev <f1v3_dev@naver.com>
2023-02-17 01:06:14 +0900

    [Update] JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,22 @@
 package hello.hellospring.domain;
 
+// JPA를 사용하기 위해서 Entity를 Mapping
+// ORM : Object Relation Mapping
+
+
+import jakarta.persistence.*;
+
+// JPA가 관리하는 Entity
+@Entity
+
 public class Member {
 
+
     // 고객이 정한 id가 아닌 시스템에서 정한 임의의 id
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY) // DB가 알아서 생성하는 PK
     private Long id;
+
+//    @Column(name = "username")
     private String name;
 
     public Long getId() {
